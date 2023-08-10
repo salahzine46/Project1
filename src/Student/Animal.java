8c30fc24547ec9f6144d916e40422ad6e4eef29e
salah <salah@salahs-MacBook-Air.local>
2023-08-09 21:53:22 -0400

    our sicth commit

--- src/Student/Animal.java
+++ src/Student/Animal.java
@@ -4,5 +4,7 @@ public class Animal {
     public static void main(String[] args) {
         System.out.println("hello guys");
     }
-    public void method1
+    public void method1(){
+        System.out.println("Hello world");
+    }
 }
